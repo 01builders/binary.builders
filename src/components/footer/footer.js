b68301fb363c8e28929655933919d6f739f152ca
MoonMan421 <len.seitter@gmail.com>
2023-03-17 14:24:16 +0530

    add footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,9 +8,44 @@ import styles from './footer.module.scss';
 const Footer = (props) => {
   return (
     <div className={styles.footer}>
-      <Typography className={styles.copyright} color="white">
-        &copy;&nbsp;{' '}{new Date().getFullYear()}&nbsp;BINARY HOLDINGS
-      </Typography>
+      <div className={styles.category}>
+        <Typography className={styles.title} color="white" href={'https'}>
+            Social Media
+        </Typography>
+        <a href='https://twitter.com/binary_builders' style={{ textDecoration: 'none' }}>
+          <Typography className={styles.item} color="white">
+              Twitter
+          </Typography>
+        </a>
+      </div>
+      <div className={styles.category}>
+        <Typography className={styles.title} color="white">
+            Ventures
+        </Typography>
+        <a href='https://v1.cosmos.network/sdk' style={{ textDecoration: 'none' }}>
+          <Typography className={styles.item} color="white">
+              Cosmos SDK
+          </Typography>
+        </a>
+        <a href='https://numia.xyz/' style={{ textDecoration: 'none' }}>
+          <Typography className={styles.item} color="white">
+              Numia
+          </Typography>
+        </a>
+        <a href='https://join.builders/' style={{ textDecoration: 'none' }}>
+          <Typography className={styles.item} color="white">
+              Interchain Builders Program
+          </Typography>
+        </a>
+      </div>
+      <div className={styles.category}>
+        <Typography className={styles.title} color="white">
+            Legal
+        </Typography>
+        <Typography className={styles.item} color="white">
+          &copy;&nbsp;{' '}{new Date().getFullYear()}&nbsp;BINARY BUILDERS
+        </Typography>
+      </div>
     </div>
   )
 }
