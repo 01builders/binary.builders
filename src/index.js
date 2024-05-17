4a06e892b0f0c5f5e7fe2f47681d844db3cbdacc
MoonMan421 <len.seitter@gmail.com>
2024-05-17 16:38:26 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import BinaryBuilders from './binary-builders';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 
 ReactDOM.render(
