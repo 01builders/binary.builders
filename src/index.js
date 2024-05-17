c037ec4a02a3c332319c0f06f40110bf6f37299d
MoonMan421 <len.seitter@gmail.com>
2024-05-17 16:04:58 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import BinaryBuilders from './binary-builders';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BinaryBuilders />
+    <Router basename="/">
+      <BinaryBuilders />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
