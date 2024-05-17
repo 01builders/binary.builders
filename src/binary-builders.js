d16747afdca42954fe9375c0a99aa06d28362b5d
MoonMan421 <len.seitter@gmail.com>
2024-05-17 16:06:25 +0200

    add routes

--- src/binary-builders.js
+++ src/binary-builders.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 
 import Landing from './views/landing/landing';
+import ContactForm from './components/ContactForm';
 
 import styles from './binary-builders.module.scss';
 
@@ -12,7 +13,10 @@ const BinaryBuilders = () => {
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <Header />
-        <Landing />
+        <Routes>
+          <Route path='/contact' element={<ContactForm className={styles.form}/>}/>
+          <Route path='*' element={<Landing/>}/>
+        </Routes>
         <Footer />
       </div>
     </div>
