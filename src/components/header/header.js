f19d9a8a15e47e1ca1bf7ad8ba1e4142497198e8
MoonMan421 <len.seitter@gmail.com>
2024-06-21 12:44:29 +0200

    fix header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,39 +1,36 @@
 import React from 'react';
 
 import Typography from '../typography';
-import {useNavigate} from "react-router-dom"
+import {useNavigate, Link} from "react-router-dom"
 
 import { ReactComponent as BinaryLogo } from '../../assets/header-logo.svg';
 
 import styles from './header.module.scss';
+import email from '../icons/png/email-icon.png';
 
 const Header = (props) => {
 
   let navigate = useNavigate()
 
-  const redirectToContact = () => {
-    navigate("../contact");
-  }
 
   return (
     <div className={styles.header}>
       <Typography className={styles.logo} component="a" href="/">
         <BinaryLogo className={styles['header-logo']}/>
         <Typography className={styles['header-title']} color="white" component="span">B1NARY BUILDERS</Typography>
+        <Typography className={styles['header-title-mobile']} color="white" component="span">B1NARY BUILDERS</Typography>
       </Typography>
-      <Typography className={styles['display-mobile']} component="div">
-        <a  onClick={() => {
-              redirectToContact()
-            }} 
-            className={styles.inside} 
-            target='_self' 
-            rel="noreferrer" 
-            href=''
-            style={{ textDecoration: 'none' }}>
+      <Typography className={styles['display-desktop']} component="div">
+          <Link className={styles['header-contact']}  to="/contact">
           <Typography color="white" component="span">
             Contact Us
           </Typography>
-        </a>
+          </Link>
+      </Typography>
+      <Typography className={styles['display-mobile']} component="div">
+          <Link className={styles['header-contact']}  to="/contact">
+          <img src={email} alt="EMAIL" className={styles['header-icon']} />
+          </Link>
       </Typography>
     </div>
   )
