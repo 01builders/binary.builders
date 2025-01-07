d610754bbc001bcb6cca6ec975e328250487a405
oezguercelebi <oezguercelebi@gmail.com>
2025-01-07 10:41:36 +0100

    Delete old

--- src/components/inputs/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Input } from './input/input';
-export { default as Textarea } from './textarea/textarea';
-export { default as Frank } from './input/frank';
-export { default as Checkbox } from './checkbox/checkbox';
-export { default as Select } from './select/select';
-export { default as SearchableSelect } from './searchable-select/searchable-select';
-export * from './selection-card';
